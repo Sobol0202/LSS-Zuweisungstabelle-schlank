78feb243d786da97817c35eca361d94853273543
Sobol0202 <67016865+Sobol0202@users.noreply.github.com>
2024-04-08 14:28:32 +0200

    Update LSS Zuweisungstabelle schlank.user.js
    +added Script-Rerun to modify Table after changing any Buttons

--- LSS Zuweisungstabelle schlank.user.js
+++ LSS Zuweisungstabelle schlank.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LSS Zuweisungstabelle schlank
 // @namespace    www.leitstellenspiel.de
-// @version      0.1
+// @version      0.9
 // @description  Macht die Personalzuweisungstabelle in den Fahrzeugen etwas schlanker
 // @author       MissSobol
 // @match        https://www.leitstellenspiel.de/vehicles/*/zuweisung
@@ -18,40 +18,67 @@
         }
     `);
 
-    // Wähle die Tabelle mit der id "personal_table"
-    var personalTable = document.getElementById("personal_table");
+    // Funktion zum Aktualisieren des Button-Stylings
+    function updateButtonStyling() {
+        //console.log("Update Button Styling");
 
-    // Überprüfe, ob die Tabelle existiert
-    if (personalTable) {
-        // Wähle alle Zeilen in der Tabelle
-        var rows = personalTable.getElementsByTagName("tr");
+        // Wähle die Tabelle mit der id "personal_table"
+        var personalTable = document.getElementById("personal_table");
 
-        // Iteriere über alle Zeilen in der Tabelle
-        for (var i = 0; i < rows.length; i++) {
-            // Wähle das letzte td-Element in der aktuellen Zeile
-            var lastTd = rows[i].getElementsByTagName("td")[rows[i].getElementsByTagName("td").length - 1];
+        // Überprüfe, ob die Tabelle existiert
+        if (personalTable) {
+            // Wähle alle Zeilen in der Tabelle
+            var rows = personalTable.getElementsByTagName("tr");
 
-            // Überprüfe, ob das letzte td-Element existiert und ob es einen Button enthält
-            if (lastTd && lastTd.querySelector("a.btn")) {
-                // Füge der Klasse des Buttons "btn-xs" hinzu
-                lastTd.querySelector("a.btn").classList.add("btn-xs");
+            // Iteriere über alle Zeilen in der Tabelle
+            for (var i = 0; i < rows.length; i++) {
+                // Wähle das letzte td-Element in der aktuellen Zeile
+                var lastTd = rows[i].getElementsByTagName("td")[rows[i].getElementsByTagName("td").length - 1];
 
-                // Wähle den Button im letzten td-Element
-                var button = lastTd.querySelector("a.btn");
+                // Überprüfe, ob das letzte td-Element existiert und ob es einen Button enthält
+                if (lastTd && lastTd.querySelector("a.btn")) {
+                    //console.log("Button gefunden");
 
-                // Wähle den Text im letzten td-Element
-                var text = lastTd.querySelector("a:not(.btn)");
+                    // Wähle den Button im letzten td-Element
+                    var button = lastTd.querySelector("a.btn");
 
-                // Überprüfe, ob sowohl Button als auch Text existieren
-                if (button && text) {
-                    // Entferne den Zeilenumbruch zwischen Button und Text
-                    if (button.nextSibling.nodeName === "#text") {
-                        button.parentNode.removeChild(button.nextSibling);
+                    // Überprüfe, ob die Klasse des Buttons "btn-xs" vorhanden ist
+                    if (!button.classList.contains("btn-xs")) {
+                        //console.log("Button Klasse wird aktualisiert");
+                        // Füge der Klasse des Buttons "btn-xs" hinzu
+                        button.classList.add("btn-xs");
+
+                        // Wähle den Text im letzten td-Element
+                        var text = lastTd.querySelector("a:not(.btn)");
+
+                        // Überprüfe, ob sowohl Button als auch Text existieren
+                        if (button && text) {
+                            //console.log("Button und Text vorhanden");
+
+                            // Entferne den Zeilenumbruch zwischen Button und Text
+                            if (button.nextSibling && button.nextSibling.nodeName === "#text") {
+                                //console.log("Entferne Zeilenumbruch");
+                                button.parentNode.removeChild(button.nextSibling);
+                            }
+                            // Drehe die Reihenfolge von Button und Text um
+                            lastTd.insertBefore(button, text);
+                            //console.log("Button-Styling aktualisiert");
+                        }
                     }
-                    // Drehe die Reihenfolge von Button und Text um
-                    lastTd.insertBefore(button, text);
                 }
             }
         }
     }
+
+    // Funktion zum regelmäßigen Überprüfen von Änderungen am Button
+    function checkForButtonChanges() {
+        // Rufe die Funktion zum Aktualisieren des Button-Stylings auf
+        updateButtonStyling();
+
+        // Überprüfe alle 1 Sekunden auf Änderungen am Button
+        setTimeout(checkForButtonChanges, 1000);
+    }
+
+    // Starte die Überprüfung von Änderungen am Button
+    checkForButtonChanges();
 })();
